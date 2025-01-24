10021c88fd6fb412ea93a3939ac83ed81f16fdd4
Vincent Carrier <vincentcarrier15@gmail.com>
2025-01-24 09:49:19 -0500

    Remove CustomEvents

--- src/lib/dvd-menu.svelte.ts
+++ src/lib/dvd-menu.svelte.ts
@@ -1,36 +1,32 @@
 import type { Action } from 'svelte/action'
 
-const dvdMenu: Action<HTMLDivElement> = (node) => {
+const dvdMenu: Action<HTMLDivElement, { speed: number }> = (node, { speed }) => {
 	let x = $state(node.offsetLeft)
 	let y = $state(node.offsetTop)
-	let dx = $state(0)
-	let dy = $state(0)
+	let dx = $state(speed)
+	let dy = $state(speed)
 	let width = $state(node.offsetWidth)
 	let height = $state(node.offsetHeight)
 	let interval: any = $state()
 	let xSoundFx = new Audio('/bounce.wav')
 	let ySoundFx = new Audio('/bounce.wav')
 
-	function onDvdStart(ev: any) {
-		dx = ev.detail.speed
-		dy = ev.detail.speed
-		interval = setInterval(() => {
-			x += dx
-			y += dy
-			node.style.left = `${x}px`
-			node.style.top = `${y}px`
+	interval = setInterval(() => {
+		x += dx
+		y += dy
+		node.style.left = `${x}px`
+		node.style.top = `${y}px`
 
-			if (x <= -2 || x >= window.innerWidth + 2 - width) {
-				dx *= -1
-				xSoundFx.play()
-			}
-			if (y <= -2 || y >= window.innerHeight + 2 - height) {
-				dy *= -1
-				ySoundFx.play()
-			}
-		}, 16)
-	}
-	node.addEventListener('dvdstart', onDvdStart)
+		if (x <= -2 || x >= window.innerWidth + 2 - width) {
+			dx *= -1
+			xSoundFx.play()
+		}
+		if (y <= -2 || y >= window.innerHeight + 2 - height) {
+			dy *= -1
+			ySoundFx.play()
+		}
+	}, 16)
+
 	$effect(() => {
 		return () => {
 			clearInterval(interval)
--- src/routes/contact-me.svelte
+++ src/routes/contact-me.svelte
@@ -4,23 +4,10 @@
 	import dvdMenu from '$lib/dvd-menu.svelte'
 	import { scale } from 'svelte/transition'
 	import { linear } from 'svelte/easing'
-	let win = $state() as HTMLElement
 	let handle = $state() as HTMLElement
 	let active = $state(true)
 	let closeCount = $state(0)
 	let { class: className } = $props()
-
-	$effect(() => {
-		if (active && closeCount) {
-			handle.dispatchEvent(new CustomEvent('draggableoff', { bubbles: true }))
-			handle.dispatchEvent(
-				new CustomEvent('dvdstart', {
-					detail: { speed: closeCount * 1.5 },
-					bubbles: true
-				})
-			)
-		}
-	})
 
 	function onclick() {
 		active = false
@@ -31,56 +18,66 @@
 	}
 </script>
 
-{#if active}
+{#if closeCount == 0}
 	<div
-		class={['flex-y-center fixed w-96', className]}
-		bind:this={win}
 		use:draggable={{ handle }}
-		use:dvdMenu
+		class={['flex-y-center fixed w-96', className]}
 		transition:scale={{ duration: 200, easing: linear, opacity: 1.0 }}
 	>
-		<div class="border-nested z-10 -mb-4 flex w-[22rem] bg-slate-50">
-			<h2
-				class="w-full px-2 text-lg font-bold tracking-tight text-slate-950 uppercase select-none"
-				bind:this={handle}
-			>
-				Contact me
-			</h2>
-			<button class="origin-center scale-[2.0] transform cursor-pointer" {onclick}>
-				<X class="relative ml-auto origin-center  scale-50 transform" />
-			</button>
-		</div>
-		<form class="border-nested flex-y-1 w-full bg-slate-950 px-3 py-4 text-slate-100">
-			<label for="name">Name</label>
-			<input
-				type="text"
-				id="name"
-				name="name"
-				autocomplete="name"
-				placeholder="Dennis Richie"
-				required
-			/>
-			<label for="email">Email</label>
-			<input
-				type="email"
-				id="email"
-				name="email"
-				autocomplete="email"
-				placeholder="dennis@bell.labs"
-				required
-			/>
-			<label for="message">Message</label>
-			<textarea
-				id="message"
-				name="message"
-				rows="4"
-				required
-				class="bg-base w-full border-2 border-slate-950 p-2 text-sm outline-1 -outline-offset-4 outline-slate-950"
-			></textarea>
-			<button type="submit" class="btn self-end"> Send --> </button>
-		</form>
+		{@render contactMe()}
+	</div>
+{:else if active}
+	<div
+		use:dvdMenu={{ speed: closeCount * 1.5 }}
+		class={['flex-y-center fixed w-96', className]}
+		transition:scale={{ duration: 200, easing: linear, opacity: 1.0 }}
+	>
+		{@render contactMe()}
 	</div>
 {/if}
+
+{#snippet contactMe()}
+	<div class="border-nested z-10 -mb-4 flex w-[22rem] bg-slate-50">
+		<h2
+			class="w-full px-2 text-lg font-bold tracking-tight text-slate-950 uppercase select-none"
+			bind:this={handle}
+		>
+			Contact me
+		</h2>
+		<button class="origin-center scale-[2.0] transform cursor-pointer" {onclick}>
+			<X class="relative ml-auto origin-center  scale-50 transform" />
+		</button>
+	</div>
+	<form class="border-nested flex-y-1 w-full bg-slate-950 px-3 py-4 text-slate-100">
+		<label for="name">Name</label>
+		<input
+			type="text"
+			id="name"
+			name="name"
+			autocomplete="name"
+			placeholder="Dennis Richie"
+			required
+		/>
+		<label for="email">Email</label>
+		<input
+			type="email"
+			id="email"
+			name="email"
+			autocomplete="email"
+			placeholder="dennis@bell.labs"
+			required
+		/>
+		<label for="message">Message</label>
+		<textarea
+			id="message"
+			name="message"
+			rows="4"
+			required
+			class="bg-base w-full border-2 border-slate-950 p-2 text-sm outline-1 -outline-offset-4 outline-slate-950"
+		></textarea>
+		<button type="submit" class="btn self-end"> Send --> </button>
+	</form>
+{/snippet}
 
 <style lang="postcss">
 	@reference '../app.css';
