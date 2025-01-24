cf69908d6c85e50ef94147a49ac0b98a70c69e4c
Vincent Carrier <vincentcarrier15@gmail.com>
2025-01-24 00:42:45 -0500

    Scaffold projects section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,81 +6,56 @@
 	let more = $state(false)
 </script>
 
-<div class="border-nested my-32">
-	<header class="flex-y-center bg-slate-950 p-4 text-slate-50">
-		<h1 class="flex-x-1 text-6xl font-black tracking-tighter uppercase">
-			<div class="-mt-[6px]">Vincent</div>
-			/
-			<div class="mt-1">Carrier</div>
-		</h1>
-		<p class="mt-1 text-4xl font-bold">&lt;web-developer/&gt;</p>
-		<nav class="flex-x-4 my-4 justify-center">
-			{#each [{ name: 'About', href: '/' }, { name: 'Projects', href: '/projects' }, { name: 'Blog', href: '/blog' }] as { name, href }, i}
-				<a {href} class="btn hover:-outline-offset-[6px]">
-					{i}/ {name} -->
-				</a>
-			{/each}
-		</nav>
-	</header>
-
-	<main>
-		<div class="flex max-md:flex-col">
-			<div
-				class="flex-y h-full w-1/2 bg-slate-50 px-4 py-4 outline-1 -outline-offset-4 outline-slate-800 md:h-96"
-			>
-				<h2># README</h2>
-				<p>
-					I'm a web developer living in Montréal. I work with startups, agencies and clients to ship
-					websites which are delightful, fast and on-budget.
-				</p>
-				<p>
-					I work mainly with front-end technologies like React and TypeScript, but I'm comfortable
-					with backend languages like Python and PHP.
-				</p>
-				<p>I'm currently available for new projects. Feel free to reach out below.</p>
-				<button class="btn mt-4 cursor-pointer self-end" onclick={() => (more = true)}
-					>more about me --></button
-				>
-			</div>
-			<img src="/portrait.png" alt="" class="w-1/2" />
-		</div>
-		{#if more}
-			<div
-				class="flex flex-row-reverse max-md:flex-col"
-				in:slide={{ easing: linear, duration: 200 }}
-			>
-				<div
-					class="flex-y h-full w-1/2 bg-slate-50 px-8 py-4 outline -outline-offset-4 outline-slate-800 md:h-96"
-				>
-					<h2>## IF YOU INSIST</h2>
-					<p>
-						I've been writing code for over 10 years. I'm a big Linux fan and I spend most of time
-						inside a terminal window.
-					</p>
-					<p class="grow">
-						Outside of coding, I'm a huge language nerd who's currently learning Ancient Greek. I
-						also love board games, swimming, biking, and the occasional meditation retreat.
-					</p>
-					<button class="btn mt-4 self-end">more !!! --></button>
-				</div>
-				<img src="/landscape.png" alt="" class="w-1/2" />
-			</div>
-		{/if}
-	</main>
+<div class="flex max-md:flex-col">
+	<div
+		class="flex-y h-full w-1/2 bg-slate-50 px-4 py-4 outline-1 -outline-offset-4 outline-slate-800 md:h-96"
+	>
+		<h2># README</h2>
+		<p>
+			I'm a web developer living in Montréal. I work with startups, agencies and clients to ship
+			websites which are delightful, fast and on-budget.
+		</p>
+		<p>
+			I work mainly with front-end technologies like React and TypeScript, but I'm comfortable with
+			backend languages like Python and PHP.
+		</p>
+		<p>I'm currently available for new projects. Feel free to reach out below.</p>
+		<button class="btn mt-4 cursor-pointer self-end" onclick={() => (more = true)}
+			>more about me --></button
+		>
+	</div>
+	<img src="/portrait.png" alt="" class="w-1/2" />
 </div>
+{#if more}
+	<div class="flex flex-row-reverse max-md:flex-col" in:slide={{ easing: linear, duration: 200 }}>
+		<div
+			class="flex-y h-full w-1/2 bg-slate-50 px-8 py-4 outline -outline-offset-4 outline-slate-800 md:h-96"
+		>
+			<h2>## IF YOU INSIST</h2>
+			<p>
+				I've been writing code for over 10 years. I'm a big Linux fan and I spend most of time
+				inside a terminal window.
+			</p>
+			<p class="grow">
+				Outside of coding, I'm a huge language nerd who's currently learning Ancient Greek. I also
+				love board games, swimming, biking, and the occasional meditation retreat.
+			</p>
+			<button class="btn mt-4 self-end">more !!! --></button>
+		</div>
+		<img src="/landscape.png" alt="" class="w-1/2" />
+	</div>
+{/if}
 
 <ContactMe class="right-12 bottom-12" />
 
 <style lang="postcss">
 	@reference '../app.css';
 
-	main {
-		h2 {
-			@apply mb-2 text-2xl font-bold;
-		}
-		p {
-			@apply indent-[1ch] leading-snug text-pretty hyphens-auto;
-			hyphenate-limit-chars: 6 3 3;
-		}
+	h2 {
+		@apply mb-2 text-2xl font-bold;
+	}
+	p {
+		@apply indent-[1ch] leading-snug text-pretty hyphens-auto;
+		hyphenate-limit-chars: 6 3 3;
 	}
 </style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { ExternalLink } from 'lucide-svelte'
+
+	const projects = [
+		{
+			name: 'Oxytone',
+			description: 'Learning dead tongues the new way',
+			technologies: 'Python, Web Components',
+			href: 'https://oxytone.xyz/'
+		},
+		{
+			name: 'molsoft.io',
+			description: 'Marketing website for the Molsoft eCommerce agency',
+			technologies: 'React, NextJS, TypeScript, Tailwind',
+			href: 'https://www.molsoft.io/'
+		},
+		{
+			name: 'Endossed',
+			description: 'Web app for Endossed, a FinTech startup',
+			technologies: 'React, TypeScript, Tailwind',
+			href: 'https://web.archive.org/web/20221115131319/https://endossed.com/'
+		},
+		{
+			name: 'Greek Learns You',
+			description: "Learn Greek words you didn't know you already knew",
+			technologies: 'Svelte, Tailwind',
+			href: 'https://greek-learns-you.netlify.app/'
+		}
+	]
+</script>
+
+<div class="flex flex-col max-md:flex-col">
+	{#each projects as { name, description, technologies, href }, i}
+		<div class="flex-x h-40 even:flex-row-reverse">
+			<div
+				class="flex-y w-full bg-slate-50 px-4 py-4 outline-1 -outline-offset-4 outline-slate-800"
+			>
+				<h2 class="text-lg font-bold">
+					{name} <a {href} class="-mb-1 inline-block w-8"><ExternalLink /></a>
+				</h2>
+				<p class="grow">{description}</p>
+				<p class="font-light italic">{technologies}</p>
+			</div>
+			<img src="" alt="" class="w-96 bg-slate-400" />
+		</div>
+	{/each}
+</div>
